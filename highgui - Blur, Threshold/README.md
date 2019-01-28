52a46a5595b768cd10d94fd905b61547fb19d013
hemanth346 <hemanth346@gmail.com>
2019-01-28 20:24:51 +0530

    Updated gui blur threshold

---
+++ highgui - Blur, Threshold/README.md
@@ -0,0 +1,23 @@
+# Blur and Threshold images on the go
+A simple API to see effects of different blurring techniques, thresholding functions on the image. Individual effects and combine effects on the image can be visualized.
+
+Watch the video here:
+
+## Requirements
+- Python
+- cv2
+
+## Usage
+```
+python gui_blur_threhsold.py -i <image_path>
+```
+
+## Output examples
+
+<img src="src\image.png" alt="Output Fig. 1" width="500"/>
+
+<img src="src\image.png" alt="Output Fig. 2" width="500"/>
+
+<img src="src\image.png" alt="Output Fig. 3" width="500"/>
+
+<img src="src\image.png" alt="Output Fig. 4" width="500"/>
--- highgui - Blur, Threshold/gui_blur_threshold.py
+++ highgui - Blur, Threshold/gui_blur_threshold.py
@@ -3,23 +3,25 @@ import cv2
 import argparse
 
 parse = argparse.ArgumentParser()
-parse.add_argument("-i","--image",help="Provide path to image")
+parse.add_argument("-i","--image",required=True,help="Provide path to image")
 args = vars(parse.parse_args())
 
-image = cv2.imread(args["image"])
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-global_blurred = cv2.blur(gray,(2,2))
-_,global_thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV)
 
-def blur_track(val):
-    '''
-    Function call everytime value is changed in Trackbar
-        In order to facilate combined effect of image blur and thresholding
-    '''
-    output,text = blur_img(gray)
-    cv2.putText(output, text, (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,0), 1)
-    cv2.imshow(title_window_blur,output)
-    combined()
+try:
+    image = cv2.imread(args["image"])
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    global_blurred = cv2.blur(gray,(2,2))
+    _,global_thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV)
+
+    screen_res = 1280, 720
+    scale_width = screen_res[0] / image.shape[1]
+    scale_height = screen_res[1] / image.shape[0]
+    scale = min(scale_width, scale_height)
+    out_width = int(image.shape[1] * scale)
+    out_height = int(image.shape[0] * scale)
+    output_dim = (out_width, out_height)
+except:
+    exit("Please check filename or if only image is given as input")
 
 def blur_img(image):
     '''
@@ -52,16 +54,6 @@ def blur_img(image):
     text = name + '-kernel:' + str(ksize)
     #cv2.putText(img, text , (20,20),cv2.FONT_HERSHEY_SIMPLEX,0.7,(192,234,123),1)
     return blurred,text
-
-def thresh_track(val):
-    '''
-    Function call everytime value is changed in Trackbar
-        In order to facilate combined effect of image blur and thresholding
-    '''
-    output,name = thresh_img(gray)
-    cv2.putText(output, name, (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (192,234,123), 1)
-    cv2.imshow(title_window_thresh,output)
-    combined()
 
 def thresh_img(image):
     '''
@@ -104,12 +96,35 @@ def thresh_img(image):
     global_thresh = thresh.copy()
     return thresh, name
 
+def blur_track(val):
+    '''
+    Function call everytime value is changed in Trackbar
+        In order to facilate combined effect of image blur and thresholding
+    '''
+    output,text = blur_img(gray)
+    output = cv2.resize(output, output_dim)
+    cv2.putText(output, text, (int(out_width * 0.1), int(out_height*0.2)), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,0), 2)
+    cv2.imshow(title_window_blur,output)
+    combined()
+
+def thresh_track(val):
+    '''
+    Function call everytime value is changed in Trackbar
+        In order to facilate combined effect of image blur and thresholding
+    '''
+    output,name = thresh_img(gray)
+    output = cv2.resize(output, output_dim)
+    cv2.putText(output, name, (int(out_width * 0.1), int(out_height*0.2)), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,0), 2)
+    cv2.imshow(title_window_thresh,output)
+    combined()
+
 def combined():
     '''
     Display combined smoothing and thresholding effect on images
     '''
     thresh_img(global_blurred)
-    cv2.imshow(title_window_combined, global_thresh)
+    output = cv2.resize(global_thresh, output_dim)
+    cv2.imshow(title_window_combined, output)
 
 title_window_blur = "Play with blur"
 title_window_thresh = "Play with threshold"
@@ -125,15 +140,15 @@ max_kernel_size = 10
 max_thresh_types = 7
 max_thresh_value = 255
 
-cv2.namedWindow(title_window_blur)
+cv2.namedWindow(title_window_blur,cv2.WINDOW_NORMAL)
 cv2.createTrackbar(title_trackbar_blur, title_window_blur, 2, max_blur_types, blur_track)
 cv2.createTrackbar(title_trackbar_kernel, title_window_blur, 1, max_kernel_size, blur_track)
 
-cv2.namedWindow(title_window_thresh)
+cv2.namedWindow(title_window_thresh,cv2.WINDOW_NORMAL)
 cv2.createTrackbar(title_trackbar_thresh_type, title_window_thresh, 1, max_thresh_types, thresh_track)
 cv2.createTrackbar(title_trackbar_thresh_value, title_window_thresh, 128, max_thresh_value, thresh_track)
 
-cv2.namedWindow(title_window_combined)
+cv2.namedWindow(title_window_combined,cv2.WINDOW_NORMAL)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
